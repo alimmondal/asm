59582b2bbfa7593fbb3cc0cec1af6b07a449bc6a
alimmondal <italimbd@gmail.com>
2024-10-06 17:50:21 +0600

    উপদ

--- src/pages/hscItems/Pronoun.tsx
+++ src/pages/hscItems/Pronoun.tsx
@@ -1,29 +1,27 @@
 const Pronoun = () => {
   return (
-    <div className="flex items-center justify-center h-screen bannerBg text-7xl">
-      <div className="max-w-4xl mx-auto bg-white p-8 rounded-lg shadow-md">
-        <h1 className="text-2xl font-bold mb-4">Question No: 08</h1>
-        <h2 className="text-xl font-semibold mb-2">
-          Clear the unclear pronoun
-        </h2>
-        <h3 className="text-lg font-semibold">Pronoun</h3>
-        <p className="mb-4">
-          **Noun এর পরিবর্তে যে সকল শব্দ ব্যবহার হয় তাকে pronoun বলে।
-          <br />
-          **Pronoun আটকে প্রকার হতে পারে
-        </p>
-        <ul className="list-disc list-inside mb-4">
-          <li>Personal: he, she, i, we, they, you.</li>
-          <li>Impersonal: each, every, any, one.</li>
-          <li>Reflexive: himself, herself, themself.</li>
-          <li>Reciprocal: each other, one another.</li>
-          <li>Possessive: his, her, our, my, their.</li>
-          <li>Relative: who, what, which, whom.</li>
-          <li>Interrogative: what, whom, whose, where.</li>
-          <li>Demonstrative: this, that, those, these.</li>
-        </ul>
+    <div className="px-3 md:w-[85%] mx-auto">
+      <h1 className="text-2xl font-bold ">Question No: 08</h1>
+      <h2 className="text-xl font-semibold ">Clear the unclear pronoun</h2>
+      <h3 className="text-lg font-semibold">Pronoun</h3>
+      <p className="">
+        **Noun এর পরিবর্তে যে সকল শব্দ ব্যবহার হয় তাকে pronoun বলে।
+        <br />
+        **Pronoun আটকে প্রকার হতে পারে
+      </p>
+      <ul className="list-disc list-inside ">
+        <li>Personal: he, she, i, we, they, you.</li>
+        <li>Impersonal: each, every, any, one.</li>
+        <li>Reflexive: himself, herself, themself.</li>
+        <li>Reciprocal: each other, one another.</li>
+        <li>Possessive: his, her, our, my, their.</li>
+        <li>Relative: who, what, which, whom.</li>
+        <li>Interrogative: what, whom, whose, where.</li>
+        <li>Demonstrative: this, that, those, these.</li>
+      </ul>
 
-        <table className="table-auto border-collapse border border-gray-500 w-full mb-4">
+      <div className="w-full my-5 overflow-x-auto">
+        <table className="md:w-full table-auto border-collapse border border-gray-500 min-w-[500px] overflow-x-auto">
           <thead>
             <tr>
               <th className="border border-gray-500 px-4 py-2">Subjective</th>
@@ -85,34 +83,114 @@ const Pronoun = () => {
             </tr>
           </tbody>
         </table>
+      </div>
 
-        <div className="mb-4">
-          <h4 className="font-semibold">
-            1. Verb এর আগে Subject বলে এবং verb এর পরে object বলে।
-          </h4>
-          <ul className="list-disc list-inside">
-            <li>I have a pen.</li>
-            <li>Let me and he go home.</li>
-          </ul>
-        </div>
+      <div className="">
+        <h4 className="font-semibold">
+          1. Verb এর আগে Subject বলে এবং verb এর পরে object বলে।
+        </h4>
+        <ul className="list-disc list-inside">
+          <li>I have a pen.</li>
+          <li>Let me and he go home.</li>
+        </ul>
+      </div>
 
-        <div className="mb-4">
-          <h4 className="font-semibold">
-            2. He/ him/ his দ্বারা পুলক বাচক কুবাযা কিন্তু she/ her/ her দ্বারা
-            মহিলাকে বুঝায়।
-          </h4>
-          <p>
-            **Salam and Salma are siblings; Salam is in className five who was
-            her sister, Salma, reads in className three.
-          </p>
-        </div>
+      <div className="">
+        <h4 className="font-semibold">
+          2. He/ him/ his দ্বারা পুলক বাচক কুবাযা কিন্তু she/ her/ her দ্বারা
+          মহিলাকে বুঝায়।
+        </h4>
+        <p>
+          ** Salam and Salma are siblings; Salam is in className five who was
+          her sister, Salma, reads in className three.
+        </p>
 
-        <div className="mb-4">
-          <h4 className="font-semibold">
-            3. বাকের Subject অনুলারে possessive করে।
-          </h4>
-          <p>**Salma answered that she knew his mother's name.</p>
-        </div>
+        <h4 className="font-semibold">
+          3. বাকের Subject অনুসারে possessive case বসে।
+        </h4>
+        <p>**Salma answered that she knew his mother's name.</p>
+
+        <p>
+          <strong>1.</strong> Salam and Salma are siblings. They go to school by
+          rickshaw because it is five kms away from their home.
+        </p>
+        <p>
+          <strong>➔</strong> They go to school by rickshaw because the school is
+          away from their home.
+        </p>
+
+        <p>
+          <strong>2.</strong> Traveling is a part of education. This means going
+          from one place to another.
+        </p>
+        <p>
+          <strong>➔</strong> Traveling is a part of education. This means going
+          from one place to another.
+        </p>
+
+        <p>
+          <strong>9.</strong> ইতর প্রাণী বা কোন বস্তুর পরিবর্তে ইট/ its ব্যবহার
+          হয়। <br />
+          Example:
+        </p>
+        <p>
+          <strong>➔</strong> The dog is faithful to its master.
+        </p>
+        <p>
+          <strong>➔</strong> The dog is faithful to its master.
+        </p>
+
+        <p>
+          <strong>10.</strong> Verb এবং preposition এর পরে pronoun এর objective
+          case বসে। Example:
+        </p>
+        <p>
+          <strong>➔</strong> Bina's aunt took great care of their.
+        </p>
+        <p>
+          <strong>➔</strong> Bina's aunt took care of them.
+        </p>
+        <p>
+          <strong>➔</strong> His teacher loved he very much.
+        </p>
+        <p>
+          <strong>➔</strong> His teacher loved him very much.
+        </p>
+
+        <p>
+          <strong>11.</strong> বাক্যের Subject এবং object এবং possessive case
+          উদাহরণ:
+        </p>
+        <p>
+          <strong>➔</strong> My parents tried my best to bring me up well in my
+          childhood.
+        </p>
+        <p>
+          <strong>➔</strong> My parents tried their best to bring me up well in
+          my childhood.
+        </p>
+
+        <p>
+          <strong>12.</strong> বাক্যে that/this/it বসে:
+        </p>
+        <p>
+          <strong>➔</strong> That was 19th February and it was Sadia's birthday.
+        </p>
+        <p>
+          <strong>➔</strong> This was 19th February and it was Sadia's birthday.
+        </p>
+
+        <p>
+          <strong>13.</strong> Noun এর পূর্বে pronoun এর possessive case বসে:
+        </p>
+        <p>
+          <strong>➔</strong> Milton took him M.A degree at the age of 24 and
+          went to him father's house in the village of Horton.
+        </p>
+        <p>
+          <strong>➔</strong> Milton took his M.A degree at the age of 24 and
+          went to his father's house in the village of Horton.
+        </p>
       </div>
     </div>
   );
