5b575d2dd1c88d362d7d6626478f6ba0422506c4
alimmondal <italimbd@gmail.com>
2024-10-06 21:17:30 +0600

    upd

--- src/pages/DegreeItems/DCompleting.tsx
+++ src/pages/DegreeItems/DCompleting.tsx
@@ -1,7 +1,282 @@
-function DCompleting() {
+const DCompleting = () => {
   return (
-    <div className="h-screen flex items-center justify-center">DCompleting</div>
+    <div className="px-5 md:w-[85%] mx-auto">
+      <div className="">
+        <div className="relative text-9xl italic text-green-400">
+          <p>C</p>
+        </div>
+        <div className="absolute top-60 left-48">
+          <h1 className="text-xl font-bold italic text-green-500">
+            ompleting Sentence
+          </h1>
+        </div>
+      </div>
+
+      <div className="py-4">
+        <h1 className="text-2xl font-bold mb-4">For Degree</h1>
+        <h2 className="text-xl font-semibold mb-2">Completing Sentence</h2>
+        <p className="mb-4">for degree / honors</p>
+        <ol className="list-decimal list-inside">
+          <li className="mb-2">
+            Of, for, from, on, in, into, by, about, without, before, after -এর
+            পর preposition -এর পর verb-এর present form ব্যবহার হয়।
+            <span className="font-semibold"> Example:</span> I went to field for
+            playing football.
+          </li>
+          <li className="mb-2">
+            Preposition ‘to’ এর পর verb-এর present form ব্যবহার হয়।
+            <span className="font-semibold"> Example:</span> want to meet you.
+          </li>
+          <li className="mb-2">
+            Look forward to (প্রত্যাশা করা), with a view to (দৃষ্টিতে রাখা),
+            prefer to (পছন্দ করা), get used to (অভ্যস্ত হওয়া) -এর পর verb-এর ing
+            ব্যবহার হয়।
+            <span className="font-semibold"> Example:</span> I am looking
+            forward to hearing you.
+            <span className="font-semibold"> Example:</span> He is used to
+            living in cold climate now.
+          </li>
+          <li className="mb-2">
+            Active voice - a subject-এর পর am, is, are, was, were, shall, will,
+            be, may be, should be, must be, can be, might be, ought to be
+            ব্যবহার হয় verb-এর base form এর সাথে।
+            <span className="font-semibold"> Example:</span> Kamala is sleeping
+            now.
+          </li>
+          <li className="mb-2">
+            Passive voice - a subject-এর পর am, is, are, was, were, shall, will,
+            be, can be, could be, may be, might be, must be, should be ব্যবহার
+            হয় verb-এর past participle।
+            <span className="font-semibold"> Example:</span> (সক্রিয় সত্তা হতে)
+          </li>
+          <li className="mb-2">
+            Active voice - a can, could, should, would, will, shall, may, might,
+            must ব্যবহার হয় verb-এর base form এর সাথে।
+            <span className="font-semibold"> Example:</span> I will go home.
+          </li>
+          <li className="mb-2">
+            Active voice - a can have, could have, should have, will have, would
+            have, may have, might have, must have ব্যবহার হয় verb-এর past
+            participle এর সাথে।
+          </li>
+          <li className="mb-2">
+            Passive voice - a can have been, could have been, shall have been,
+            should have been, may have been, might have been, must have been
+            ব্যবহার হয় verb-এর past participle এর সাথে।
+          </li>
+          <li className="mb-2">
+            If clause Present tense-এর পর future tense-এর clause হয়।
+            <span className="font-semibold"> Example:</span> If you come, I will
+            go.
+          </li>
+          <li className="mb-2">
+            If clause past indefinite tense-এর পর future conditional clause হয়।
+            <span className="font-semibold"> Example:</span> If you came, I
+            would go.
+          </li>
+          <li>
+            If clause - <span className="font-semibold">যদি</span> past perfect
+            tense-এ হয় তবে
+            <span className="font-semibold">
+              পরবর্তী clause -এ could have, would have, might have
+            </span>
+            ব্যবহার হয়।
+            <span className="italic">
+              উদাহরণ: If you had come, I would have gone.
+            </span>
+          </li>
+          <li>
+            Have to, has to, had to, will have to এর present form হল।
+            <span className="italic">উদাহরণ: I have to go market.</span>
+          </li>
+          <li>
+            While এর পর <span className="font-semibold">verb</span> এর সাথে{" "}
+            <span className="font-semibold">ing</span>
+            যোগ করা হয়।
+            <span className="italic">
+              উদাহরণ: While waking on the road, I saw a dead cow.
+            </span>
+          </li>
+          <li>
+            Article ‘The’ এর পর <span className="font-semibold">noun</span> বসে।
+            <span className="italic">
+              উদাহরণ: The destruction of forest is dangerous.
+            </span>
+          </li>
+          <li>
+            Co-ordinate conjunction (যেমন: and, but, or, nor, not only....but
+            also)
+            <span className="font-semibold"> clause</span> এর conjunction এর পরে
+            verb-টি
+            <span className="font-semibold"> tense-এ হবে</span>, পরে verb-টি
+            <span className="font-semibold"> tense-এ হবে</span>।
+            <span className="italic">
+              উদাহরণ: I went to market and bought a shirt.
+            </span>
+          </li>
+          <li>
+            Having- এর পর <span className="font-semibold">verb</span> এর past
+            participle হয়।
+            <span className="italic">
+              উদাহরণ: Having done the work, I went home.
+            </span>
+          </li>
+          <li>
+            Cannot but, Could not but -এর পর{" "}
+            <span className="font-semibold">verb</span> এর present form হয়।
+            <span className="italic">উদাহরণ: I cannot but laugh.</span>
+          </li>
+          <li>
+            Cannot help, Could not help -এর পর{" "}
+            <span className="font-semibold">verb</span> এর
+            <span className="font-semibold"> present form</span> হয়।
+            <span className="italic">উদাহরণ: I cannot help laughing.</span>
+          </li>
+          <li>
+            Used to- এর পর <span className="font-semibold">verb</span> এর
+            present from হয়।
+            <span className="italic">উদাহরণ: I used to go to school.</span>
+          </li>
+          <li>
+            Be used to -এর পর <span className="font-semibold">verb</span> এর
+            সাথে <span className="font-semibold">ing</span>
+            হয়।
+            <span className="italic">উদাহরণ: I am used to smoking.</span>
+          </li>
+          <li>
+            One of, each of the -এর পর{" "}
+            <span className="font-semibold">plural noun</span>
+            <span className="font-semibold">বহুবচন</span> হয়।
+            <span className="italic">
+              উদাহরণ: One of my friends went there.
+            </span>
+          </li>
+          <li>
+            Despite, besides, in spite of -এর পর{" "}
+            <span className="font-semibold">verb</span> এর
+            <span className="font-semibold"> present form</span> হয়।
+            <span className="italic">
+              উদাহরণ: In spite of working hard, he failed.
+            </span>
+          </li>
+        </ol>
+      </div>
+
+      <div className="">
+        <div className="">
+          <h2>Exercise</h2>
+        </div>
+        <ol className="list-decimal ">
+          <li>
+            It is high time you <span className="font-semibold">gave up</span>
+            your bad habit.
+          </li>
+          <li>
+            A little learning is a
+            <span className="font-semibold">dangerous</span> thing.
+          </li>
+          <li>
+            I <span className="font-semibold">like</span> traveling.
+          </li>
+          <li>
+            The girl is afraid <span className="font-semibold">of</span>
+            insects.
+          </li>
+          <li>
+            You should not hanker <span className="font-semibold">after</span>
+            money.
+          </li>
+          <li>
+            He takes pride <span className="font-semibold">in</span> his wealth.
+          </li>
+          <li>
+            Listen <span className="font-semibold">to</span> what I say.
+          </li>
+          <li>
+            He is addicted <span className="font-semibold">to</span> gambling.
+          </li>
+          <li>
+            He prefers coffee <span className="font-semibold">to</span> tea.
+          </li>
+          <li>
+            Hurry up <span className="font-semibold">or</span> you will be late.
+          </li>
+          <li>
+            Slow and steady <span className="font-semibold">wins</span> the
+            race.
+          </li>
+          <li>
+            I saw them <span className="font-semibold">swimming</span> in the
+            river.
+          </li>
+          <li>
+            <span className="font-semibold">Prevention</span> is better than
+            cure.
+          </li>
+          <li>
+            She behaves as if she <span className="font-semibold">were</span>{" "}
+            mad.
+          </li>
+          <li>
+            He is intelligent <span className="font-semibold">enough</span> to
+            understand the meaning.
+          </li>
+          <li>
+            <span className="font-semibold">Without</span> reading more, you
+            can’t pass.
+          </li>
+          <li>
+            <span className="font-semibold">Look</span> before you leap.
+          </li>
+          <li>
+            The doctor <span className="font-semibold">felt</span> the pulse of
+            the patient.
+          </li>
+          <li>
+            Better late <span className="font-semibold">than</span> never.
+          </li>
+          <li>
+            You had <span className="font-semibold">better</span> leave the
+            room.
+          </li>
+          <li>
+            A rolling stone <span className="font-semibold">gathers</span> no
+            moss.
+          </li>
+          <li>
+            Charity <span className="font-semibold">begins</span> at home.
+          </li>
+          <li>
+            The boy is too <span className="font-semibold">weak</span> to walk.
+          </li>
+          <li>
+            I wish you <span className="font-semibold">were</span> my sister.
+          </li>
+          <li>
+            Two and two <span className="font-semibold">make</span> four.
+          </li>
+          <li>
+            Would you mind <span className="font-semibold">having</span> a cup
+            of tea?
+          </li>
+          <li>
+            As you <span className="font-semibold">sow</span>, so you reap.
+          </li>
+          <li>
+            We cannot <span className="font-semibold">do</span> without money.
+          </li>
+          <li>All that glitters is not gold.</li>
+          <li>The more, the better.</li>
+          <li>
+            Pen <span className="font-semibold">through</span> the line.
+          </li>
+          <li>
+            <span className="font-semibold">Put off</span> your shoes.
+          </li>
+        </ol>
+      </div>
+    </div>
   );
-}
+};
 
 export default DCompleting;
