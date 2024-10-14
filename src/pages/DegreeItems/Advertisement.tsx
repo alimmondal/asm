c9b67ef3e168f73da00f00ecd534e710bb6640bb
alimmondal <italimbd@gmail.com>
2024-10-14 21:39:25 +0600

    upd

--- src/pages/DegreeItems/Advertisement.tsx
+++ src/pages/DegreeItems/Advertisement.tsx
@@ -1,7 +1,66 @@
 function Advertisement() {
   return (
-    <div className="h-screen flex items-center justify-center">
-      Advertisement
+    <div className="h-full md:w-[85%] mx-auto  ">
+      <h1 className="text-center font-semibold text-md py-20">Advertisement</h1>
+      <div className="px-3">
+        <div className="border p-2 mb-10">
+          {/* To Let Section */}
+          <div className="border p-4 mb-6 max-w-md mx-auto">
+            <h3 className="font-semibold underline mb-4 text-center">To Let</h3>
+            <ul className="list-disc pl-6">
+              <li>Aristocratic Area.</li>
+              <li>Overlooking Lake.</li>
+              <li>All time Security.</li>
+              <li>Near School/College/Varsity/Market/Hospital.</li>
+              <li>Pollution Free.</li>
+              <li>Beside Highway.</li>
+              <li>Reasonable Price/Rent.</li>
+              <li>3 Bed, 2 Bath, 1 Spacious Dining.</li>
+            </ul>
+            <p className="mt-4 font-semibold text-end">Please Contact:</p>
+            <p className="text-end">Mobile: 017513337773</p>
+          </div>
+
+          {/* Rent A Car Section */}
+          <div className="border p-4 mb-6 max-w-md mx-auto">
+            <h3 className="font-semibold underline mb-4 text-center">
+              Rent A Car
+            </h3>
+            <ul className="list-disc pl-6">
+              <li>All Automatic Option.</li>
+              <li>Charming Color.</li>
+              <li>Attractive Design.</li>
+              <li>Latest Model.</li>
+              <li>A/C Operated.</li>
+              <li>Owner Driven.</li>
+            </ul>
+            <p className="mt-4 font-semibold text-end">Please Contact:</p>
+            <p className="text-end">Mobile: 017513337773</p>
+          </div>
+
+          {/* First Come, First Served Section */}
+          <div className="border p-4 mb-6 max-w-md mx-auto">
+            <h3 className="font-semibold underline mb-4 text-center">
+              First Come, First Served
+            </h3>
+            <ul className="list-disc pl-6">
+              <li>Charming Color.</li>
+              <li>Attractive Design.</li>
+              <li>Latest Model.</li>
+              <li>Environment Friendly.</li>
+              <li>Money Saving.</li>
+              <li>8 Years Warranty.</li>
+              <li>Longer Lasting.</li>
+              <li>Available Spare parts.</li>
+            </ul>
+            <p className="mt-4 font-semibold text-end">Please Contact:</p>
+            <p className="text-end">Mobile: 017513337773</p>
+          </div>
+        </div>
+      </div>
+      <div className="">
+        <img src="img/adv1.jpg" alt="" />
+      </div>
     </div>
   );
 }
--- src/pages/DegreeItems/PartsOfSpeech.tsx
+++ src/pages/DegreeItems/PartsOfSpeech.tsx
@@ -1,7 +1,14 @@
 function PartsOfSpeech() {
   return (
-    <div className="h-screen flex items-center justify-center">
-      PartsOfSpeech
+    <div className="w-full bg-gray-100">
+      <div className="text-center">
+        <h1 className="py-20">Parts of Speech</h1>
+      </div>
+      <div className="h-full w-[85%] mx-auto ">
+        <img src="img/parts1.jpg" alt="" />
+        <img src="img/parts2.jpg" alt="" />
+        <img src="img/parts3.jpg" alt="" />
+      </div>
     </div>
   );
 }
