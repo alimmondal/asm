c6a25c9a9d3a2f28ef6ef523057c67ded5ac1de9
alimmondal <italimbd@gmail.com>
2024-12-04 22:47:07 +0600

    upd

--- src/pages/hscItems/Preposition.tsx
+++ src/pages/hscItems/Preposition.tsx
@@ -1,8 +1,104 @@
 const Preposition = () => {
   return (
     <div className="h-full  ">
-      <div className="">
-        <h1 className="text-center font-semibold text-md py-20">Preposition</h1>
+      <div className="w-[50%] mx-auto dark:bg-white">
+        <img src="/prepositionTitle.png" alt="" className="h-24 md:h-36" />
+      </div>
+      <div className="my-10 w-[95%] mx-auto">
+        1. শুন্যস্থানের একটি/ দুটি শব্দের পূর্বে যদি a, an, the থাকে তবে
+        শূন্যস্থানে of বসে। অর্থাৎ দুটি Noun (N+N) এর মাঝখানে of বসে।
+        <br />
+        *Zahid will buy the gold. ...Arabia. =Zahid will buy the gold of Arabia.{" "}
+        <br />
+        <br />
+        2. সংখ্যার পরের শুন্যস্থানের= of বসে এবং সংখ্যার পূর্বের শুন্যস্থানে
+        about বসে। <br />
+        Mith is one.............my friends. Mim is one of my friends.
+        .........three hundred million people speak English. About three hundred
+        million people speak English. <br />
+        <br />
+        3. of এর আগের ফাকা জায়গায় =out বসে। <br />
+        *Ruhul is now out...........his room. Ruhul is now out of his room.
+        *Ruhul is now .......of his room. = Ruhul is now out of his room. <br />
+        <br />
+        4. দুটি verb এর মাঝখানের শূন্যস্থানের-To-বসে। অর্থাৎ শুন্যস্থানের পরে
+        verb থাকলে শুন্যস্থানে to বসে। কোন কিছুর দিকে যাওয়া বুঝাতে-to- বসে।
+        <br />
+        *Mim is glad......hear of her good result. -Mim is glad to hear of her
+        good result. *Mim will fly to America by plane. <br />
+        <br />
+        5. বড় স্থানের পূর্বে/স্থানের মধ্যে বুঝাতে/সালের পূর্বে/ season এর
+        পূর্বে/মাস বা মাসের নামের পূর্বে/ স্থান বা সময়রে পূর্বে a/an/the থাকলে
+        সেই স্থান বা সময়ের (a/an/the) পূর্বের শূন্যস্থানে-in বসে। *Mim lives in
+        America. <br />
+        *Mim is in her room. *Mim was reading in our college in 2011. *Mim and
+        her father work in a bank. <br />
+        <br />
+        6. ভাষার নামের পূর্বে এবং সাধারনত Be verb (am, is, are) এর পূর্বের
+        শুন্যস্থানে in বসে। <br />
+        *Mim can speak in English. *Mim is in great danger.
+        <br />
+        <br />
+        7. ছোট স্থান (জায়গার নাম) এর পূর্বে- নির্দিষ্ট সময়ের পূর্বে at বসে।{" "}
+        <br />
+        <br />
+        *Mim will stay at Tilokpur at 9pm. *কিন্তু ব্যপক সময় বুঝালে -for বসে
+        *Mim has been reading in our college for two years.
+        <br />
+        <br />
+        <br />
+        ৪. নির্দিষ্ট দিন বা দিনের নামের পূর্বে তারিখে পূর্বে, Tv, Radio,
+        Telivision, mobile ইত্যাদির পূর্বে-on বসে। <br />
+        *On Monday Mim heard the news on TV.
+        <br />
+        <br />
+        9. বাক্যের শুরুতে শুন্যস্থানের পরে যদি-ing যুক্ত verb থাকে তবে
+        শুন্যস্থানে-by/without (অর্থ বুঝে বসাতে হবে) বসে। <br />
+        *By workimg hard, Mim made a good result. *Without working, Mim cannot
+        make good result. <br />
+        <br />
+        10. V-3 এর পরে শুন্যস্থানে এবং যানবাহনের নামের পূবের শুন্যস্থানে-by বসে।
+        *Mim is always helped by me. *Mim will fly to America by plane.
+        <br />
+        <br />
+        <br />
+        <h3 className=""> EXTRA HITS:</h3>
+        <br />
+        <br />
+        er-যুক্ত শব্দের পরে-than বসে। <br />
+        *Mim's hand is bigger than mine. <br />
+        <br />
+        Or যুক্ত শব্দের পরের শুন্যস্থানে-to- বসে। (senior, junior, major,
+        superior, exterior, ulterior, inferior) <br />
+        *Mim is senior to me by four years.
+        <br />
+        <br />
+        Such এর পূর্বে-in বসে। <br />
+        *Mim is in such a condition that we can't describe.
+        <br />
+        <br />
+        অংগ প্রতংগের নামের পূর্বের শূন্যস্থানে-by/in বসে। <br />
+        *Mim caught me by hand.
+        <br />
+        <br />
+        যন্ত্রপাতির নামের পূর্বের with বসে। <br />
+        *Mim beaten the snake with a stick.
+        <br />
+        <br />
+        So এর পরে by/without বসে। <br />
+        <br />
+        what এর পূর্বে-with বসে। <br />
+        *Mim should be satisfied with what she has. <br />
+        <br />
+        verb এর পরে ব্যক্তিবাচক object এর পূর্বের শুন্যস্থানে-to/for বসে। <br />
+        *Mim bought a gift for Hasan. <br />
+        শুন্যস্থানের একটি/দুটি শব্দের পরে to থাকলে শুন্যস্থানে from বসে। <br />
+        *Mim will study from morning to noon.
+        <br />
+        <br />
+        Life এর পূর্বে সাধারণত-in বসে।
+        <br />
+        *May Mim shine in life.
       </div>
       <div className="pb-10 md:w-[85%] mx-auto">
         <img src="img/prep1.jpg" alt="" />
