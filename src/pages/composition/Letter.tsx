a9fae04d319cc7ed21866d68afbac5eb6be94dc6
alimmondal <italimbd@gmail.com>
2025-03-23 11:34:49 +0600

    upd

--- src/pages/composition/Letter.tsx
+++ src/pages/composition/Letter.tsx
@@ -46,151 +46,168 @@ const Letter = () => {
         {/* Description */}
         <div className="py-5">
           <h2 className="">DESCRIPTION</h2>
-          1. Write a letter to your Friend inviting him on the occasion of the
-          marriage ceremony of your sister. <br />
-          Or. A letter to your friend to attend your birthday party.
+          <p className="">
+            1. Write a letter to your Friend inviting him on the occasion of the
+            marriage ceremony of your sister. <br />
+            Or. A letter to your friend to attend your birthday party.
+            <br />
+            Or, A letter to your friend inviting him to join a picnic.
+            <br />
+            or, A letter to your friend inviting him to spend a few days with
+            you during the summer vacation.
+            <br />
+            Or, A letter to your friend inviting him to a tea party.
+            <br />
+            <strong>Answer: </strong> You will be glad to know that during this
+            summer vacation the 10th June is the date of my
+            birth/picnic/marriage ceremony of my elder sister. We are going to
+            have a (tea) party on that occasion. (I can't even think of passing
+            this happy occasion without you). I hope that you will join us and
+            stay with us a few days to increase our pleasure. <br />
+            <br />
+          </p>
+          <p className="">
+            2. Q. A letter describing your native village. <br />
+            Or, A litter describing foods and food habits in Bangladesh. <br />
+            Or. A letter telling about Bangladesh and her people. <br />
+            Or, A letter telling you about your country.
+            <br />
+            Dr. A letter informing some customs and ways of behaving in your
+            country.
+            <br />
+            <strong>Answer: </strong> The name of my [COUNTRY] is [TILAKPUR]. It
+            is a small low-lying island on the bay of Bengal. The people of this
+            are mostly Muslim, Hindu and Christians. We love to live in peace
+            and harmony. We have three meals a day and our staple food is rice,
+            vegetables and beef. I love my country/ Village and I am proud of
+            it. <br />
+            <br />
+          </p>
+          <p className="">
+            3 Q. A letter describing a picnic/ excursion/study tour. <br />
+            or. A letter describing your experience, about a train journey.
+            <br />
+            or. A letter describing a picnic/ a place of historical interest.
+            <br />
+            or. A letter describing your visit to the national zoo. <br />
+            or. A letter describing a memorable day of your life. <br />
+            <strong>Answer: </strong> (You will be glad to know that) we went on
+            a picnic by train/boat to we enjoyed gossiping with other
+            passengers. After reaching On the way -I was really overwhelmed to
+            see various things/ scenery/ animals. However, the journey/ picnic
+            excursion in my hart I bore. It gave me much pleasure. (indeed it
+            was one of the most memorable days in my life). <br />
+            <br />
+          </p>
+          <p className="">
+            4. Q. Write a letter to your father asking to send you some money.{" "}
+            <br />
+            Or, A letter to your father telling him about your progress in your
+            studies. <br />
+            <strong>Answer: </strong> My annual exam is near at hand. You will
+            be glad to know that I have prepared well for it. But just after the
+            exam I have to buy some books. Please send me the money as soon as
+            possible. <br />
+            <br />
+          </p>
+          <p className="">
+            5. Q. A letter to your younger brother advising him to be sincere
+            and attentive to his studies.
+            <br />
+            Or. Advising him to give up the bad habit of smoking.
+            <br />
+            Or, Informing him about the bad effects of smoking.
+            <br />
+            <strong>Answer: </strong> Bad company is as bad as committing
+            suicide. Smoking harms your health and badly affects your brain and
+            bad company will spoil your career. As a result you have failed in
+            all subjects. But it is said that smoking/ keeping bad company is as
+            bad as committing suicide
+          </p>
           <br />
-          Or, A letter to your friend inviting him to join a picnic.
+
+          <p className="">
+            6. Q. Write a letter to your friend about your future plan of life.
+            <br />
+            Or, A letter about your aim in life.
+            <br />
+            Or. A letter telling about why you want to be a teacher/doctor/ good
+            politician/ social worker.
+            <br />
+            Or. A letter telling about what you intend after your graduation/
+            Degree exam.
+            <br />
+            <strong>Answer: </strong> Everybody should have an aim. So am I and
+            my aim is to become as it is a noble profession to serve the poor
+            people. So after my graduation I want to serve my nation as a
+            teacher/ social worker/ politician/ doctor.
+          </p>
+
           <br />
-          or, A letter to your friend inviting him to spend a few days with you
-          during the summer vacation.
+
+          <p className="">
+            7. Q. A letter to your friend congratulating you on her/ his
+            brilliant result/ success in the SSC/ HSC/ BA examination.
+            <br />
+            Or, A letter to your friend asking him to return the book that
+            he/she borrowed.
+            <br />
+            <strong>Answer: </strong> My joys know no bounds that you have got
+            GPA 5 in the exam. Please accept my heartiest congratulation because
+            you are really worthy of it. But I am badly in need of the book that
+            you borrowed from me, kindly return it as soon as possible.
+          </p>
+
           <br />
-          Or, A letter to your friend inviting him to a tea party.
-          <br />
-          <br />
-          <strong>Answer: </strong> You will be glad to know that during this
-          summer vacation the 10th June is the date of my birth/picnic/marriage
-          ceremony of my elder sister. We are going to have a (tea) party on
-          that occasion. (I can't even think of passing this happy occasion
-          without you). I hope that you will join us and stay with us a few days
-          to increase our pleasure. <br />
-          <br />
-          2. Q. A letter describing your native village. <br />
-          Or, A litter describing foods and food habits in Bangladesh. <br />
-          Or. A letter telling about Bangladesh and her people. <br />
-          Or, A letter telling you about your country.
-          <br />
-          Dr. A letter informing some customs and ways of behaving in your
-          country.
-          <br />
-          <br />
-          <strong>Answer: </strong> The name of my [COUNTRY] is [TILAKPUR]. It
-          is a small low-lying island on the bay of Bengal. The people of this
-          are mostly Muslim, Hindu and Christians. We love to live in peace and
-          harmony. We have three meals a day and our staple food is rice,
-          vegetables and beef. I love my country/ Village and I am proud of it.{" "}
-          <br />
-          <br />
-          <br />
-          3 Q. A letter describing a picnic/ excursion/study tour. <br />
-          or. A letter describing your experience, about a train journey.
-          <br />
-          or. A letter describing a picnic/ a place of historical interest.
-          <br />
-          or. A letter describing your visit to the national zoo. <br />
-          or. A letter describing a memorable day of your life. <br />
-          <br />
-          <strong>Answer: </strong> (You will be glad to know that) we went on a
-          picnic by train/boat to we enjoyed gossiping with other passengers.
-          After reaching On the way -I was really overwhelmed to see various
-          things/ scenery/ animals. However, the journey/ picnic excursion in my
-          hart I bore. It gave me much pleasure. (indeed it was one of the most
-          memorable days in my life). <br />
-          <br />
-          4. Q. Write a letter to your father asking to send you some money.{" "}
-          <br />
-          Or, A letter to your father telling him about your progress in your
-          studies. <br />
-          <br />
-          <strong>Answer: </strong> My annual exam is near at hand. You will be
-          glad to know that I have prepared well for it. But just after the exam
-          I have to buy some books. Please send me the money as soon as
-          possible. <br />
-          <br />
-          5. Q. A letter to your younger brother advising him to be sincere and
-          attentive to his studies.
-          <br />
-          Or. Advising him to give up the bad habit of smoking.
-          <br />
-          Or, Informing him about the bad effects of smoking.
-          <br />
-          <br />
-          <strong>Answer: </strong> Bad company is as bad as committing suicide.
-          Smoking harms your health and badly affects your brain and bad company
-          will spoil your career. As a result you have failed in all subjects.
-          But it is said that smoking/ keeping bad company is as bad as
-          committing suicide
-          <br />
-          <br />
-          6. Q. Write a letter to your friend about your future plan of life.
-          <br />
-          Or, A letter about your aim in life.
-          <br />
-          Or. A letter telling about why you want to be a teacher/doctor/ good
-          politician/ social worker.
-          <br />
-          Or. A letter telling about what you intend after your graduation/
-          Degree exam.
-          <br />
-          <br />
-          <strong>Answer: </strong> Everybody should have an aim. So am I and my
-          aim is to become as it is a noble profession to serve the poor people.
-          So after my graduation I want to serve my nation as a teacher/ social
-          worker/ politician/ doctor.
-          <br />
-          <br />
-          7. Q. A letter to your friend congratulating you on her/ his brilliant
-          result/ success in the SSC/ HSC/ BA examination.
-          <br />
-          Or, A letter to your friend asking him to return the book that he/she
-          borrowed.
-          <br />
-          <br />
-          <strong>Answer: </strong> My joys know no bounds that you have got GPA
-          5 in the exam. Please accept my heartiest congratulation because you
-          are really worthy of it. But I am badly in need of the book that you
-          borrowed from me, kindly return it as soon as possible.
-          <br />
-          <br />
-          8. Q. A letter to your friend expressing your condolence to her/his
-          father's death.
-          <br />
-          Or, A letter to your friend consoling him at his failure in the
-          SSC/HSC/BA exam.
-          <br />
-          <br />
-          <strong>Answer: </strong> I am very shocked to hear that your [
-          father/ mother ] is no more on earth/ you have failed in the exam.
-          Your grief is so great that my words will fail to console you. But
-          What is lotted cannot be blotted but your failure/ the loss of a
-          father is irrecoverable. So you should try to do better in future.
+
+          <p className="">
+            8. Q. A letter to your friend expressing your condolence to her/his
+            father's death.
+            <br />
+            Or, A letter to your friend consoling him at his failure in the
+            SSC/HSC/BA exam.
+            <br />
+            <strong>Answer: </strong> I am very shocked to hear that your [
+            father/ mother ] is no more on earth/ you have failed in the exam.
+            Your grief is so great that my words will fail to console you. But
+            What is lotted cannot be blotted but your failure/ the loss of a
+            father is irrecoverable. So you should try to do better in future.
+          </p>
+
           <br />
         </div>
 
         <div className="pt-10">
           <h2>😂 Complain Letter</h2>
           <p className="text-justify">
-            Q. Complaint about load shedding.
+            Q. Suppose, you are Karim/Kabita, one of the inhabitants of Dinajpur
+            City Corporation. Recently the roads of your locality have been
+            unusable due to heavy rainfalls and the sewerage drains are being
+            hampered to flow water. People and the school going students cannot
+            go to their offices, shops, schools and other places. Now, write a
+            letter to the Mayor of your City Corporation complaining about the
+            sufferings of the common people.
             <br />
             <br />
             <strong>Answer: </strong> <br />
             To, <br />
-            The Director, <br />
-            Divisional Power Supply Directorate, <br />
+            Dinajpur City Mayor, <br />
+            Dinajpur City Corporation, Dinajpur, <br />
             Sylhet. <br />
-            Subject: Complaint regarding excessive load shedding. <br />
+            Subject: Complaint regarding drainage system. <br />
             Sir, <br />
-            With due respect we state that, we the residents of Sylhet
-            Moulvibazar district are in indescribable suffering due to intense
-            load shedding in the heat of this summer. Almost half of the day our
-            city is without electricity. Load shedding continues for long
-            periods of time even at night. Every day due to lack of electricity
-            our daily life activities are disrupted. Sick patients are
-            suffering, students are not able to concentrate on their studies,
-            factory production is disrupted. In this situation, it becomes very
-            urgent to take action on this endless suffering. <br />
-            So, I humbly request you to take urgent steps for uninterrupted
-            power supply in Moulvibazar district. <br />
+            With due respect we state that{" "}
+            <strong>
+              [SUPPOSE, YOU এর পর থেকে Now এর আগের ফুল স্টপ পর্যন্ত].
+            </strong>{" "}
+            In this situation, it becomes very urgent to take action on this
+            endless problem. <br />
+            So, I humbly request you to take urgent steps for{" "}
+            <strong>
+              [the sufferings of the common people in Dinajpur City Corporation
+              ( বাঁ প্রশ্নের For/ to/ about এর পর থেকে শেষ পর্যন্ত)].
+            </strong>{" "}
+            <br />
             Loyal to you <br />
             Raisul Islam <br />
             For the residents of Moulvibazar district.
