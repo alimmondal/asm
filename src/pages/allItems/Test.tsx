95f5aa68089379ac5549ed120f9dcbf9ba038827
alimmondal <italimbd@gmail.com>
2025-09-12 08:09:27 +0600

    upd

--- src/pages/allItems/Test.tsx
+++ src/pages/allItems/Test.tsx
@@ -1,5 +1,14 @@
 import data from "../../../data.json";
+interface VocabularyItem {
+  word: string;
+  phonetic: string;
+  banglaPronunciation: string;
+  BengaliMeaning: string;
+  examples: string;
+  link: string;
+}
 function Test() {
+  const typedData: VocabularyItem[] = data as VocabularyItem[];
   return (
     <div className="">
       <div className="">
@@ -10,20 +19,31 @@ function Test() {
                 <tr className="bg-gray-100">
                   <th className="py-2 px-2 border-b">Words</th>
                   <th className="py-2 px-2 border-b">Phonemic transcription</th>
-                  <th className="py-2 px-2 border-b">Bangla</th>
+                  <th className="py-2 px-2 border-b">banglaPronunciation</th>
                   <th className="py-2 px-2 border-b">Meaning</th>
                   <th className="py-2 px-2 border-b">Examples</th>
                 </tr>
               </thead>
               <tbody>
-                {data.map((item: any, index: any) => (
+                {typedData.map((item: any, index: any) => (
                   <tr key={index} className="hover:bg-gray-50">
-                    <td className="py-2 px-1 border-b">{item.word}</td>
+                    <td className="py-2 px-1 border-b">
+                      <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-blue-600 hover:underline"
+                        href={item.link}
+                      >
+                        {`${index + 1}. ${item.word}`}
+                      </a>
+                    </td>
                     <td className="py-2 px-1 border-b">{item.phonetic}</td>
                     <td className="py-2 px-1 border-b">
                       {item.banglaPronunciation}
                     </td>
-                    <td className="py-2 px-1 border-b">{item.meaning}</td>
+                    <td className="py-2 px-1 border-b">
+                      {item.BengaliMeaning}
+                    </td>
                     <td className="py-2 px-1 border-b">{item.examples}</td>
                   </tr>
                 ))}
